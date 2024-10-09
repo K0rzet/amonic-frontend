de685de0fc9aed898f02f86facd665d49b4ca990
K0rzet <ehot2004@gmail.com>
2024-10-10 00:57:39 +0300

    changed port

--- src/config/get-server-url.ts
+++ src/config/get-server-url.ts
@@ -1,5 +1,5 @@
 export const getServerUrl = (path?: string) => {
 	return process.env.NODE_ENV === 'production'
 		? `https://api.example.com${path}`
-		: `http://176.124.218.145:4200${path}`
+		: `http://176.124.218.145:2223${path}`
 }
