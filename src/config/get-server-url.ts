0a2a28b33222bb8b82ff4cc46486b7421a91b83e
K0rzet <ehot2004@gmail.com>
2024-10-10 01:02:57 +0300

    fix port

--- src/config/get-server-url.ts
+++ src/config/get-server-url.ts
@@ -1,5 +1,5 @@
 export const getServerUrl = (path?: string) => {
 	return process.env.NODE_ENV === 'production'
 		? `https://api.example.com${path}`
-		: `http://176.124.218.145:2223${path}`
+		: `http://176.124.218.145:2224${path}`
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://176.124.218.145:2223/api'
+export const API_URL = 'http://176.124.218.145:2224/api'
 export const IS_CLIENT = typeof window !== 'undefined'
